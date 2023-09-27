015a3d53bb58d56d0dbfa5dff658d898a466684b
heitorflavio <heitorflavio.r@outlook.com>
2023-09-27 16:03:56 -0300

    first commit

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,155 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  <div class="memory-game">
+    <div class="grid">
+      <div v-for="(card, index) in cards" :key="index" :class="{ 'flipped': card.flipped }" @click="flipCard(index)">
+        <div class="card">
+          <div class="card-front">{{ card.value }}</div>
+          <div class="card-back"></div>
+        </div>
       </div>
-    </ion-content>
-  </ion-page>
+    </div>
+    <p> Pontos: {{ points }}</p>
+    <p>Tempo: {{ time }}</p>
+    <button @click="resetGame">Reiniciar Jogo</button>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+export default {
+  data() {
+    return {
+      cards: [],
+      flippedCards: [],
+      points: 0,
+      time: 0,
+    };
+  },
+  created() {
+    this.initializeGame();
+  },
+  methods: {
+    initializeGame() {
+      // Crie um conjunto de cartas duplicadas e embaralhe-as
+      const values = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+      const cards = values.concat(values);
+      this.cards = this.shuffleArray(cards).map((value) => ({ value, flipped: false }));
+      this.startTimer();
+    },
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
+    flipCard(index) {
+      if (!this.cards[index].flipped && this.flippedCards.length < 2) {
+        this.cards[index].flipped = true;
+        this.flippedCards.push(index);
+
+        if (this.flippedCards.length === 2) {
+          const [card1, card2] = this.flippedCards;
+          if (this.cards[card1].value === this.cards[card2].value) {
+            // As cartas coincidem, mantenha-as viradas
+            this.points = this.points + 1;
+            this.flippedCards = [];
+          } else {
+            // As cartas não coincidem, aguarde um momento e, em seguida, vire-as de volta
+            this.points = this.points - 1;
+            setTimeout(() => {
+              this.cards[card1].flipped = false;
+              this.cards[card2].flipped = false;
+              this.flippedCards = [];
+            }, 1000);
+          }
+        }
+      }
+
+      // Verifique se todos os cards estão virados
+      const allFlipped = this.cards.every((card) => card.flipped);
+      if (allFlipped) {
+        setTimeout(() => {
+          this.resetGame();
+        }, 1000);
+      }
+    },
+    resetGame() {
+      this.points = 0;
+      this.time = 0;
+      this.cards.forEach((card) => (card.flipped = false));
+      this.flippedCards = [];
+      this.initializeGame();
+    },
+    startTimer() {
+      setInterval(() => {
+        this.time = this.time += 1;
+      }, 1000);
+    },
+  },
+};
 </script>
 
+
 <style scoped>
-#container {
-  text-align: center;
-  
+.memory-game {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  gap: 10px;
+}
+
+.grid>div {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  cursor: pointer;
+  perspective: 1000px;
+}
+
+.grid>div.flipped .card {
+  transform: rotateY(0deg);
+}
+
+.card {
+  transform: rotateY(180deg);
+}
+
+.card {
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transition: transform 0.3s ease;
+}
+
+.card-front,
+.card-back {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  backface-visibility: hidden;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.card-back {
+  transform: rotateY(180deg);
+  background-color: #ccc;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 18px;
 }
 </style>
